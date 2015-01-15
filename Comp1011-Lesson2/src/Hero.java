6656b5abe82a592090c9b205e9adca0202db4b3a
chandanDadral <cdadral9@gmail.com>
2015-01-15 15:46:16 -0500

    updated hero class

--- Comp1011-Lesson2/src/Hero.java
+++ Comp1011-Lesson2/src/Hero.java
@@ -29,16 +29,28 @@ public class Hero {
 		generateAbilities();
 	}
 	
-	
-	private void generateAbilities(){
+	public void showAbilities(){
 		
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("Strength: " + this.strength);
+		System.out.println("Speed: " + this.speed);
+		System.out.println("Health: " + this.health);
+		System.out.println("+++++++++++++++++++++++++");
 	}
 	
+	private void generateAbilities(){
+	this.strength = (int) (Math.random()*100+1);
+	this.speed = (int) (Math.random()*100+1);
+	this.health = (int) (Math.random()*100+1);
+	
+	}
 	public void fight(){
+System.out.println(this.name +"is Fighting");
 		
 	}
 	
 	public void run(){
+		System.out.println(this.name +"is Running");
 		
 	}
 
