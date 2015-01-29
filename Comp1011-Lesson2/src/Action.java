091731c144915ab0eedd314e569db22ec8eab1c5
chandanDadral <cdadral9@gmail.com>
2015-01-29 11:22:08 -0500

    Made some Changes

---
+++ Comp1011-Lesson2/src/Action.java
@@ -0,0 +1,9 @@
+
+public interface Action {
+	
+	//Define required 
+		public void fight();
+		
+		public void run();
+		
+}
--- Comp1011-Lesson2/src/Hero.java
+++ Comp1011-Lesson2/src/Hero.java
@@ -1,5 +1,5 @@
 
-public class Hero {
+public class Hero implements Action{
 	//Private Properties
     protected strength;
     protected int speed;
